6a8d9163984fcae12f2f8522a59bd7e588912be6
Kitkat <mr7kitkat@gmail.com>
2022-03-16 19:26:33 +0530

    change propriate divs to actual buttons

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const targetPoint = dropdown.selectedOptions[0].innerText;
 const P1Status = document.querySelector('#p1Point');
 const P2Status = document.querySelector('#p2Point');
 // Buttons Variable
-const p1Button = document.querySelector('div.p1');
-const p2Button = document.querySelector('div.p2');
+const p1Button = document.querySelector('button.p1');
+const p2Button = document.querySelector('button.p2');
 const resetButton = document.querySelector('.reset');
 
 let p1Point = parseInt(P1Status.innerText);
@@ -24,16 +24,27 @@ let p2Point = parseInt(P2Status.innerText);
 p1Button.addEventListener('click', () => {
     p1Point += 1;
     P1Status.innerText = `${p1Point}`
+    compare()
 })
 
 p2Button.addEventListener('click', () => {
     p2Point += 1;
     P2Status.innerText = `${p2Point}`
+    compare()
 })
 
 resetButton.addEventListener('click', () => {
-    P1Status.innerText = 0;
-    P2Status.innerText = 0;
+    p1Point = 0;
+    p2Point = 0;
+    P1Status.innerText = p1Point;
+    P2Status.innerText = p2Point;
 })
 
-
+function compare() {
+    if (targetPoint == p1Point) {
+        
+    }
+    else if (targetPoint == p2Point) {
+        
+    }
+}
