de32f1c7114c3a21dfc17ca70d769b026d066f95
Kitkat <mr7kitkat@gmail.com>
2022-03-17 14:57:38 +0530

    Added Functionality to buttons to keep score

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Defining Dropdown and filling it with data
 const dropdown = document.querySelector('#dropdown');
 
 for (let i = 3; i < 12; i++) {
@@ -7,8 +8,7 @@ for (let i = 3; i < 12; i++) {
     dropdown.append(option);
 }
 
-const targetPoint = dropdown.selectedOptions[0].innerText;
-
+// All the variables
 const P1Status = document.querySelector('#p1Point');
 const P2Status = document.querySelector('#p2Point');
 // Buttons Variable
@@ -16,35 +16,70 @@ const p1Button = document.querySelector('button.p1');
 const p2Button = document.querySelector('button.p2');
 const resetButton = document.querySelector('.reset');
 
+let targetPoint = dropdown.selectedOptions[0].innerText;
 let p1Point = parseInt(P1Status.innerText);
 let p2Point = parseInt(P2Status.innerText);
 
 
-// Adding Functionality to buttons
-p1Button.addEventListener('click', () => {
-    p1Point += 1;
-    P1Status.innerText = `${p1Point}`
-    compare()
-})
+// All the functions
+function compare(target, compval) {
+    return target == compval;
+}
 
-p2Button.addEventListener('click', () => {
-    p2Point += 1;
-    P2Status.innerText = `${p2Point}`
-    compare()
-})
+function disableButtons() {
+    p1Button.disabled = true;
+    p2Button.disabled = true;
 
-resetButton.addEventListener('click', () => {
+    p1Button.style.opacity = '0.5';
+    p2Button.style.opacity = '0.5';
+}
+
+function reset() {
     p1Point = 0;
     p2Point = 0;
+    
     P1Status.innerText = p1Point;
     P2Status.innerText = p2Point;
+
+    p1Button.disabled = false;
+    p2Button.disabled = false;
+
+    p1Button.style.opacity = '1';
+    p2Button.style.opacity = '1';
+
+    P1Status.style.color = 'black';
+    P2Status.style.color = 'black';
+}
+
+// Adding Functionality to buttons
+p1Button.addEventListener('click', function () {
+    p1Point += 1;
+    P1Status.innerText = `${p1Point}`
+    let comp = compare(targetPoint, p1Point)
+    
+    if (comp) {
+        disableButtons()
+        P1Status.style.color = 'green';
+        P2Status.style.color = 'red';
+    }
 })
 
-function compare() {
-    if (targetPoint == p1Point) {
-        
+p2Button.addEventListener('click', function () {
+    p2Point += 1;
+    P2Status.innerText = `${p2Point}`
+    let comp = compare(targetPoint, p2Point)
+
+    if (comp) {
+        disableButtons()
+        P1Status.style.color = 'red';
+        P2Status.style.color = 'green';
     }
-    else if (targetPoint == p2Point) {
-        
-    }
-}
+})
+
+dropdown.addEventListener('input', function(){
+    targetPoint = this.selectedOptions[0].innerText;
+    reset()
+})
+
+
+resetButton.addEventListener('click', reset)
